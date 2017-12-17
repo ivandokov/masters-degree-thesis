943c05970baaec4d663b6b5c59ccb8667ec0cd91
Ivan Dokov <ivangdokov@gmail.com>
2017-12-17 16:33:56 +0200

    Add info windows

--- js/map.js
+++ js/map.js
@@ -17,6 +17,7 @@ function loadPlaces(callback) {
 function addMarkers(map, places) {
     for (var i = 0; i < places.length; i++) {
         var place = places[i];
+        var infoWindow = window.infoWindow(place);
         var marker = new google.maps.Marker({
             position: {lat: place.lat, lng: place.lng},
             map: map,
@@ -24,5 +25,22 @@ function addMarkers(map, places) {
             zIndex: place.index,
             animation: google.maps.Animation.DROP
         });
+        marker.addListener('click', function() {
+            infoWindow.open(map, marker);
+        });
     }
+}
+
+function infoWindow(place) {
+    var contentString = '<div>'+
+            '<h5>'+ place.name +'</h5>'+
+            '<div>'+
+                '<p>'+ place.description + '</p>' +
+                '<p class="mb-0"><a href="'+ place.moreInfoUrl +'" target="_blank">Повече информация</a></p>'+
+            '</div>'+
+        '</div>';
+
+    return new google.maps.InfoWindow({
+        content: contentString
+    });
 }
